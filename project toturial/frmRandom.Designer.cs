14ea0d05a3674c434af4c9f7da0482101c046147
SULTAN MOHAMMED ALSHEHRI <1845978@stu.kau.edu.sa>
2020-04-17 22:19:11 +0300

    Modification

---
+++ project toturial/frmRandom.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace project_toturial
+{
+    partial class frmRandom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerateRandom = new System.Windows.Forms.Button();
+            this.BtnRandomNum = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerateRandom
+            // 
+            this.btnGenerateRandom.Location = new System.Drawing.Point(234, 58);
+            this.btnGenerateRandom.Name = "btnGenerateRandom";
+            this.btnGenerateRandom.Size = new System.Drawing.Size(313, 57);
+            this.btnGenerateRandom.TabIndex = 0;
+            this.btnGenerateRandom.Text = "Generate Random Colour";
+            this.btnGenerateRandom.UseVisualStyleBackColor = true;
+            this.btnGenerateRandom.Click += new System.EventHandler(this.btnGenerateRandom_Click);
+            // 
+            // BtnRandomNum
+            // 
+            this.BtnRandomNum.Location = new System.Drawing.Point(46, 276);
+            this.BtnRandomNum.Name = "BtnRandomNum";
+            this.BtnRandomNum.Size = new System.Drawing.Size(151, 57);
+            this.BtnRandomNum.TabIndex = 1;
+            this.BtnRandomNum.Text = "Random Number Generator";
+            this.BtnRandomNum.UseVisualStyleBackColor = true;
+            this.BtnRandomNum.Click += new System.EventHandler(this.BtnRandomNum_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(573, 328);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 57);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Back";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(355, 156);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "r-g-b";
+            // 
+            // frmRandom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.BtnRandomNum);
+            this.Controls.Add(this.btnGenerateRandom);
+            this.Name = "frmRandom";
+            this.Text = "Random";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerateRandom;
+        private System.Windows.Forms.Button BtnRandomNum;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
